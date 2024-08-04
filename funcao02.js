83c4ed3b4986bd733bc2f4e145ae6b5877e360f3
castilho87 <castilhofjv1@gmail.com>
2024-08-04 18:51:07 -0300

    Aprendendo sobre funções

---
+++ funcao02.js
@@ -0,0 +1,22 @@
+mainSaveData()
+
+function mainSaveData(){
+    getData()
+    checkValues()
+    sendToDatabase()
+}
+
+// função que pega todos os dados
+function getData(){
+    console.log("Pegando dados do usuário")
+}
+
+function checkValues(){
+    console.log("Validando dados")
+
+}
+
+function sendToDatabase(){
+    console.log("Cadastrando dados")
+
+}
