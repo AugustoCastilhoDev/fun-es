2f67c59598dffb4281bca832ea2a3a361a4b8efa
castilho87 <castilhofjv1@gmail.com>
2024-08-05 14:16:52 -0300

    Exemplo de função com retorno

---
+++ funcao08.js
@@ -0,0 +1,10 @@
+//Funções com retorno
+
+let resultado = soma(5, 10)
+
+console.log("O resultado da função é " + resultado)
+
+function soma(numA, numB){
+    let somatorio = numA + numB
+    return somatorio
+}
