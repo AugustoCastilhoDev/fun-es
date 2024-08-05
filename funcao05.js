886a615d92dc1ace9ab6baf6ca0bcdd26728f216
castilho87 <castilhofjv1@gmail.com>
2024-08-05 12:01:34 -0300

    Escopo de variáveis e múltiplos parâmetros

---
+++ funcao05.js
@@ -0,0 +1,10 @@
+// funções com parâmetros
+// Escopo de variáveis e múltiplos parâmetros
+
+torrar("pao de forma", "Augusto")
+torrar("pao integral", "Lais")
+
+function torrar (pao, nome){
+    console.log("torrada feita com " + pao)
+    console.log("esse é um pedido de " + nome)
+}
